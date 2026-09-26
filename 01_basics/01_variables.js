a334431e79acc8c1b8d676f73b41db8d5670b941
shivani9205 <shivanikanojiya136@gmail.com>
2026-09-26 07:17:55 +0000

    vaiables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 144553
+let accountEmail = "Shivani@gamil.com"
+var accountPassword = "12345"
+accountCity = "Noida"
+let accountState;
+
+// accountId = 2 not allowed
+
+
+
+accountEmail = "shiva1234@gmail.com"
+accountPassword = "45787965"
+accountCity = "Bengluru"
+
+console.log(accountId);
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountCity, accountPassword, accountState])
+
+
